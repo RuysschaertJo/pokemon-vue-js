38a02df3b4c6ff1bbc66fb348508251fe69b55c6
RuysschaertJo <jo.ruysschaert@student.howest.be>
2023-11-17 16:26:43 +0100

    Push

---
+++ src/components/Pagination.vue
@@ -0,0 +1,27 @@
+<!-- Todo Layout maken -->
+<!-- Todo Pijltje terug, volgende pagina en ook totaal pagina. < x / 78 > -->
+
+<!-- Todo Prop binnen met huidige pagina -->
+<!-- Todo emit met nieuwe pagina pagina -->
+<!-- Todo functie om pagina te wisselen omhoog / omlaag niet onder 0 niet bovene limit -->
+
+<template>
+  <div class="c-pages">
+    <button class="">
+      <ChevronLeft class=""> </ChevronLeft>
+    </button>
+
+    <p class="c-page__current"></p>
+    <p class="c-page__divider"></p>
+    <p class="c-page__total">{{ 195 }}</p>
+
+    <button class="c-page__action">
+      <ChevronRight class="c-page__icon"> </ChevronRight>
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { ChevronRight, ChevronLeft } from 'lucide-vue-next';
+
+</script>
