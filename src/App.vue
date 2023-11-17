cad6e9753e282bf0a00a61ebfd60304a34674f46
RuysschaertJo <jo.ruysschaert@student.howest.be>
2023-11-17 16:16:49 +0100

    Le push

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,10 @@
   <header>
     <LogoPokemon></LogoPokemon>
     <div class="c-pok">
-      <SinglePokemon 
-      v-for="pokemon in pokemons.results" 
-      :key="pokemon.name" 
-      :pokemonData="pokemon"> 
-      <!-- key betekent dat het kan weten wat wat is.  -->
-    </SinglePokemon>
+      <LoadingSkeletons v-if="loading" :amount="20"></LoadingSkeletons>
+      <SinglePokemon v-else v-for="pokemon in pokemons.results" :key="pokemon.name" :pokemonData="pokemon">
+        <!-- key betekent dat het kan weten wat wat is.  -->
+      </SinglePokemon>
     </div>
   </header>
 
@@ -17,17 +15,20 @@
 </template>
 
 <script setup>
+import LoadingSkeletons from './components/LoadingSkeletons.vue';
 import LogoPokemon from './components/Logopokemon.vue';
 import SinglePokemon from './components/Pokemon.vue';
 import { ref } from 'vue';
 
 const pokemons = ref();
+const loading = ref(true);
 
 const getPokemons = async function (limit = 20) {
   const data = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=0`).then((r) => r.json());
   // console.log(data);
 
   pokemons.value = data;
+  loading.value = false;
   console.log(pokemons.value);
 };
 
---
+++ src/components/LoadingSkeletons.vue
@@ -0,0 +1,97 @@
+<template>
+  <!-- <p>Loading</p> -->
+
+  <div v-for="skeleton in amount" class="c-skeleton">
+    <div class="c-skeleton__image"></div>
+
+    <div class="c-skeleton__info">
+      <div class="c-skeleton__header">
+        <div class="c-skeleton__name"></div>
+        <div class="c-skeleton__button"></div>
+      </div>
+
+      <p class="c-skeleton__link">More info <ChevronRight></ChevronRight></p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ChevronRight } from 'lucide-vue-next';
+
+//Todo We willen niet 1 zien maar een x aantal die we meegeven als prop.
+defineProps({
+  amount: {
+    type: Number,
+    default: 12,
+  },
+});
+
+//Todo zet hier een prop op die een getal doorgeeft.
+</script>
+
+<style lang="scss">
+.c-skeleton {
+  width: 100%;
+
+  &__image {
+    width: 7rem;
+    height: 7rem;
+    background: #444;
+    border-radius: 2rem;
+    margin-bottom: -0.5rem;
+  }
+
+  &__info {
+    display: flex;
+    flex-direction: column;
+    background: #666;
+    color: #111111;
+    padding: 0.5rem 1rem;
+    border-radius: 1rem 2rem 1rem 2rem;
+  }
+
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__name {
+    width: 65%;
+    height: 2.25rem;
+    margin: 0;
+    border-radius: 0.5rem;
+    background-color: #777;
+  }
+
+  &__button {
+    width: 3rem;
+    height: 3rem;
+    background: #777;
+    border-radius: 50%;
+  }
+
+  &__link {
+    display: flex;
+    align-items: center;
+    padding: 1rem 0;
+    color: #dfdfdf;
+    font-size: 0.75rem;
+    margin: 0 0 0 auto;
+    text-decoration: underline;
+    gap: 0.5rem;
+  }
+}
+
+@media screen and (min-width: 480px) {
+  .c-skeleton {
+    width: calc((100% - 1 * 2rem) / 2);
+  }
+}
+
+@media screen and (min-width: 768px) {
+  .c-skeleton {
+    width: calc((100% - 3 * 2rem) / 4);
+  }
+}
+</style>
